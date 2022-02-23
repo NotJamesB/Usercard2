4735888aac971a8de404588bddfdd4643010b469
NotJamesB <97855508+NotJamesB@users.noreply.github.com>
2022-02-23 14:44:50 -0800

    Add files via upload

---
+++ User Card/card.js
@@ -0,0 +1,18 @@
+var nameswitch = document.querySelector(".names")
+
+function swap() {
+    nameswitch.innerText = "James B"
+}
+
+var element = document.querySelector(".connectionbodytext")
+console.log(element)
+
+function removee(element) {
+    element.remove()
+}
+
+var element2 = document.querySelector(".connectionbodytext2")
+
+function remove2(element2) {
+    element2.remove()
+}
